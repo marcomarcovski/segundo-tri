bbbf106b0f238cf52a5a94389f5a2c9e53e3ff8b
marcomarcovski <161604330+marcomarcovski@users.noreply.github.com>
2024-07-25 07:47:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,56 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+const caixaPrincipal= document.querySelector(".caixa-principal");
+const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultado= document.querySelector(".caixa-resultado");
+const textoResultado= document.querySelector(".texto-resultado");
 
 const perguntas = [
-    {
-        enunciado: "Qual sua opinião sobre o chat gpt?",
-            alternativas: [
-        {
-              texto:"alternativa 1"  
-              afirmação:"afirmação "
+  {
+    enunciado:"Qual o melhor mapa na opinião do Matheus"
+      alternativas: [
+  {
+ texto: "Breeze";
+afirmaçao: "afirmaçao";
 }
 {
-   texto:"alternativa 2")
-  
+  texto: "Sunset";
 
-      ]      
-        ]
-    },
-    {
-        enunciado: "Com a descoberta desta tecnologia (IA), o professor decidiu fazer uma sequência de aulas sobre esta ela. No fim de uma aule ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
-        alternativas: [
-            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-        ]
-    },
-    {
-        enunciado: "Após a elaboração do trabalho, o professor realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: [
-            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
-        ]
-    },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            "Criar uma imagem utilizando um gerador de imagem de IA."
-        ]
-    },
-    {
-        enunciado:"bolsonaro ou lula?",
-        alternativas: [
-            "sim"
-            "não",
-        ]
-    },
+
+            ]
+},
+{
+    enunciado:"Qual o agente mais divertido de jogar?"
+      alternativas: [
+        "Reyna";
+        "Jett";
+            ]
+},
+{
+    enunciado:"Qual foi o peak do Matheus?"
+      alternativas: [
+        "Imortal 1";
+        "Ascendente 3";
+            ]
+},
+{
+    enunciado:"Qual o main do Matheus?"
+      alternativas: [
+        "Jett";
+        "Clove";
+            ]
+},
+{
+    enunciado:"perguntas 5"
+      alternativas: [
+        "alternativa 1";
+        "alternativa 2";
+            ]
+},
 ];
 
 let atual = 0;
 let perguntaAtual;
-let historiaFinal ="";
+
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
@@ -61,15 +60,10 @@ function mostraPergunta() {
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto:
-        botaoAlternativas.addEventlistener("click",()=>repostaselecionada(alternativa));
+        botaoAlternativas.textContent = alternativa;
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
-function respostaselecionada(opcaoselecionada){
-    const afirmacaoes = opcaoselecionada.afirmacoes
-    historiafinal = afirmacoes;
-    atual++;
-    mostrapergunta();
-}
+
 mostraPergunta();
+            
